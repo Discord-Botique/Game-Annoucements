e4b0ce7a78be680599b48d9d94dcfdf4139871b3
Luis Augusto <hello@luis.app>
2023-05-08 10:10:52 -0700

    Initial commit

---
+++ module.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    TOKEN: string;
+    CLIENT_ID: string;
+    LOGTAIL_KEY: string;
+    SUPABASE_KEY: string;
+    SUPABASE_URL: string;
+    SUPABASE_DB: string;
+  }
+}
