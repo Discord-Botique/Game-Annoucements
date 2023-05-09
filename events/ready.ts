cc635234d825e47d93ad11cb3a528509c997b373
Luis Augusto <hello@luis.app>
2023-05-09 16:15:24 -0700

    disable send updates

--- events/ready.ts
+++ events/ready.ts
@@ -1,14 +1,14 @@
 import type { Event } from "./event";
 import { ActivityType } from "discord-api-types/v10";
 import { logtail } from "../utils/logtailConfig";
-import { sendUpdates } from "../utils/sendUpdates";
+// import { sendUpdates } from "../utils/sendUpdates";
 
 export const ready: Event<"ready"> = {
   name: "ready",
   once: true,
   async execute(client) {
     await logtail.debug(`Logged in as ${client.user.tag}!`);
-    await sendUpdates(client);
+    // await sendUpdates(client);
     client.user.setPresence({
       activities: [
         {
