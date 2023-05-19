26ace55aa711f34b29d6d2e36cb02b1cf8d77089
Luis Augusto <hello@luis.app>
2023-05-19 14:20:18 -0700

    add help text

--- commands/help.ts
+++ commands/help.ts
@@ -20,7 +20,8 @@ export const help: Command = {
         "\n" +
         "- `/subscribe steam [id-or-url]* [role-mention]` - Subscribe to a game on Steam by either the ID of the game or by pasting in the URL. You can also choose a role in your server to be notified of updates when a new announcement is made for the game. Announcements will be made on the channel the command was run on.\n" +
         "- `/unsubscribe steam [id-or-url]*` - Unsubscribe to a game from Steam that was previously subscribed to in the channel the command was run on.\n" +
-        "- `/list` - Display all current subscriptions for the server, with the channels they are active on along with the roles that will be tagged." +
+        "- `/list` - Display all current subscriptions for the server, with the channels they are active on along with the roles that will be tagged.\n" +
+        "- `/help` - Outputs this message for reference if you ever need to look up a command! \n" +
         "Hope you enjoy Game Announcements and happy gaming!",
       ephemeral: true,
     });
