e16f86a65f8775a2af5363677d3f76e0bb100d6b
Luis Augusto <hello@luis.app>
2025-01-09 14:32:35 -0800

    Add gateway intents and connect event

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 import { ready } from "@events/ready";
-import { Client, ClientEvents } from "discord.js";
+import { Client, ClientEvents, GatewayIntentBits, Partials } from "discord.js";
 import type { Event } from "@events/types";
 import { logtail } from "@utils/logtail";
 import { interactionCreate } from "@events/interactionCreate";
+import { voiceStateUpdate } from "@events/voiceStateUpdate";
 
 const client = new Client({
   allowedMentions: {
     parse: ["everyone", "roles", "users"],
   },
   // https://discord.com/developers/docs/topics/gateway#list-of-intents
-  intents: [],
+  intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds],
   // https://discordjs.guide/popular-topics/partials.html#enabling-partials
   partials: [],
 });
 
-const events: Event<keyof ClientEvents>[] = [ready, interactionCreate];
+const events: Event<keyof ClientEvents>[] = [
+  ready,
+  interactionCreate,
+  voiceStateUpdate,
+];
 
 events.forEach((event) => {
   // The ready event should only run once, when the app is ready
